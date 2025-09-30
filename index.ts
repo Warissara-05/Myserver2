99f0e5bab851d2f0bf0018ff6631058d8e189804
Warissara-05 <196065122+Warissara-05@users.noreply.github.com>
2025-09-30 14:13:09 +0700

    first time

---
+++ index.ts
@@ -0,0 +1,12 @@
+import Express = require("express");
+
+const app = Express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});

4c7d7a2921ca8197ceb6db0003300b42ce226f82
Warissara-05 <196065122+Warissara-05@users.noreply.github.com>
2025-09-30 16:06:15 +0700

    up

--- dist/Test1.js
+++ dist/Test1.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const Utils = require("./Utils")();
+const Utils = require("./Utils");
 if (Utils.add(2, 3) === 5) {
     console.log(0);
 }

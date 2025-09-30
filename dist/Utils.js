639b60ae2f5b6fbd7d3eaa319204b798a189837e
Warissara-05 <196065122+Warissara-05@users.noreply.github.com>
2025-09-30 16:01:18 +0700

    ttt

---
+++ dist/Utils.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function helloword() {
+    console.log("hello word");
+}
+function add(a, b) {
+    return a + b;
+}
+function Utils() {
+    return {
+        helloword,
+        add
+    };
+}
+module.exports = Utils;
+//# sourceMappingURL=Utils.js.map
